c14e8e4ad995602893b980cc2287b1e24a15fb4b
Pierre <sotopierre1@gmail.com>
2026-04-30 10:30:04 -0400

    Se importo el header de medico, se importo lo necesario para ejecutar el llamdo del usuario/medico que esta en la sesion, asi llamar el nombre y notificaciones

--- app/src/app/features/medico/perfil/perfil.page.ts
+++ app/src/app/features/medico/perfil/perfil.page.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent } from '@ionic/angular/standalone';
+import { AuthService } from '../../../core/services/auth.service';
 import { MedicoBottomNavComponent } from '../../../shared/components/medico-bottom-nav/medico-bottom-nav.component';
+import { MedicoHeaderComponent } from '../../../shared/components/medico-header/medico-header.component';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, MedicoBottomNavComponent]
+  imports: [IonContent, CommonModule, FormsModule, MedicoHeaderComponent, MedicoBottomNavComponent]
 })
 export class PerfilPage implements OnInit {
+  private readonly authService = inject(AuthService);
 
-  constructor() { }
+  user = this.authService.getCurrentUser();
 
   ngOnInit() {
   }
